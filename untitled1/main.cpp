60f6f7c3649b639b51fff238622632611302bcfa
sadler419 <danielh.sadler@icloud.com>
2025-02-09 12:11:10 +0000

    Add files via upload

---
+++ untitled1/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+int main() {
+    std::cout << "ANIMAL COMPANY NUTS GENERATOR" << std::endl;
+    std::cout << "WELCOME TO ANIMAL COMPANY NUTS GENERATOR" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Enter your Username" << std::endl;
+    std::string username;
+    std::getline(std::cin, username);
+
+    std::cout << "\033[31mThis Username is Valid: " << username << "\033[0m" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "\033[32mGENERATING NUTS AND MODERATOR ITEMS TO ACCOUNT! ! ! !" << std::endl;
+    std::cout << "[+] RPG INFNITIE" << std::endl;
+    std::cout << "[+] REVOLVER ESP INF ASMMO CHEATER" << std::endl;
+    std::cout << "[+] META DATER HAXS" << std::endl;
+    std::cout << "[+] ANIMAL COMPANY CHEATS .EXE" << std::endl;
+    std::cout << "[+] 98289 CUM " << std::endl;
+    std::cout << "[+] RAW USER DATER IP" << std::endl;
+    std::cout << "[+] SLOMNO GYATT MONKE" << std::endl;
+    std::cout << "[+] ARMABETA HATERS" << std::endl;
+    std::cout << "[+] ANIMAL COMPANY CHEAT MEAT DATER U=GUI APK" << std::endl;
+    std::cout << "[+] Pingul ULTIMATE GYATT HAT" << std::endl;
+    std::cout << "[+] ADMIN MOD HAT COSMETC" << std::endl;
+    std::cout << "[+] SADLER ULTIMATE META DATER CHEATER ! ! !" << std::endl;
+    std::cout << "Done" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "DO YOU ALSO WANT MODERATOR IN THE DISCORD, TYPE YES IF SO" << std::endl;
+    std::string answer;
+    std::getline(std::cin, answer);
+
+    std::cout << "\033[34mThis answer is accepted: " << answer << "\033[0m" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "ENTER YOUR CARD DETAILS HERE ! ! !" << std::endl;
+    std::string CardDetails;
+    std::getline(std::cin, CardDetails);
+
+    std::cout << "\033[32mTHANKS FOR THE DETAILS, WE WILL NOT BE IN TOUCH ! ! !: " << CardDetails << "\033[0m" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Do you also want pINGUL EPIC MOCHA V1 CHEAT? ? ? ? ?" << std::endl;
+    std::string response;
+    std::getline(std::cin, response);
+    std::cout <<"[+]GENERATING PINGUL GYATT CHEAT" << std::endl;
+    std::cout <<"[+]adding pingul toes" << std::endl;
+    std::cout <<"[+]adding pingul bumhole" << std::endl;
+    std::cout <<"[+]adding pingul femboy pics" << std::endl;
+    std::cout <<"[+]adding femboy cat ears" << std::endl;
+    std::cout <<"[+]adding coach booty" << std::endl;
+    std::cout <<"[+]adding white hacker man hat" << std::endl;
+    std::cout <<"[+]creating mochav1 gui cheat quest only" << std::endl;
+    std::cout <<"[+]generating meta data.exe file" << std::endl;
+    std::cout <<"ITEMS GENERATED" << std::endl;
+    std::cout <<
+
+
+
+    return 0;
+}
